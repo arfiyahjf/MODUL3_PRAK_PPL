99cb15334f44512715d3844e966d4b27d2c262df
arfiyahjf <arfiyaah@gmail.com>
2025-04-24 16:27:39 +0700

    first commit

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     * @group login
+     */
+    
+    public function testLogin(): void
+    {
+        $this->browse(callback: function (Browser $browser): void {
+            $browser->visit(urk:'/')
+                    ->assertSee(text:'modul3')
+                    ->clicklink(link:'Log in')
+                    ->assertPathIs(path:'/login')
+                    ->type(field:'email', value: 'admin@gmail.com')
+                    ->type(field:'password', value: 'password')
+                    ->press(button:'LOG IN')
+                    ->assertPathIs(path:'/dashboard');
+        });
+    }
+}
